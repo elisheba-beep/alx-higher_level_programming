7d2b44e6af440f36a959355855edd857746b2ad8
elisheba-beep <ekunife35@gmail.com>
2022-06-28 16:51:41 +0100

    combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(i + 1,10):
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if (i == 8 and j == 9):
+            i = str(i)
+            j = str(j)
+            print("{:s}".format(i+j))
+            continue
         if (i != j):
             i = str(i)
             j = str(j)
