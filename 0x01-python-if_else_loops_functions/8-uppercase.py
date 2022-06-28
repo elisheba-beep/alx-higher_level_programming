6ddc927ec65e561065f507c47f2b697f1ac41373
elisheba-beep <ekunife35@gmail.com>
2022-06-28 17:26:15 +0100

    lower to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 def uppercase(str):
     for i in range(ord('a'), ord('z') + 1):
         for j in range(ord('A'), ord('Z') + 1):
-            str[i] = str[j]
-            print("{:s}".format(str))
+            if (i == 32 + j):
+                str[i] = str[j]
+                print("{:s}".format(str))
