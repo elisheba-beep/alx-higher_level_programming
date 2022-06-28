5af298fd696451f065546a756541b9ccde877b61
elisheba-beep <ekunife35@gmail.com>
2022-06-28 17:59:48 +0100

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,5 @@ def print_last_digit(number):
     num = repr(number)
     last_digit_string = num[-1]
     last_digit = int(last_digit_string)
-    if (number < 0):
-        last_digit = last_digit * -1
     print("{:d}".format(last_digit), end="")
     return last_digit
