d3fefd3188ec1a8ce99aa331e2081e82ced83f28
elisheba-beep <ekunife35@gmail.com>
2022-06-28 13:28:17 +0100

    double numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,7 +4,6 @@ for num in range(0, 100):
         num = "0" + str(num)
         print("{:s}, ".format(num), end="")
         continue
-    if (num == 99):
-        print("{:d}".format(num))
+    if (num == 100):
         break
     print("{:d}, ".format(num), end="")
