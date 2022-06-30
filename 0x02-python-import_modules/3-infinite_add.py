0fa7c428226c6dd8561872540d966c0ec3867b90
elisheba-beep <ekunife35@gmail.com>
2022-06-30 14:37:24 +0100

    infinite arguments add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    """gives the addition of all arguments"""
+    add = 0
+    for i in range(1, len(sys.argv)):
+        a = int(sys.argv[i])
+        add+=a
+    print(add)
+
