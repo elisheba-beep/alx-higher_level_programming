4606a2e858dace98dfe316cbd0d5dcecdee244cf
elisheba-beep <ekunife35@gmail.com>
2022-06-30 17:27:16 +0100

    bytecoding

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
         from magic_calculation_102 import add, sub
 		if a < b:
 			c = add(a, b)
-		for i in range(4, 90):
+		for i in range(4, 6):
 			c = add(c, i)
-			return c
-		return sub(a, b)
+			return (c)
+        else:
+		    return sub(a, b)
