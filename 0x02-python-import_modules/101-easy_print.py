95997e4fd5867f4e8df581fd537040cef7a81577
elisheba-beep <ekunife35@gmail.com>
2022-06-30 17:28:37 +0100

    byte code

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/use/bin/python3
-import os
 os.write(1, b'#pythoniscool')
---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,23 @@
+# uncompyle6 version 3.8.0
+# Python bytecode 3.8.0 (3413)
+# Decompiled from: Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)]
+# Embedded file name: hidden_4.py
+# Compiled at: 2021-07-16 18:16:33
+# Size of source mod 2**32: 146 bytes
+
+
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
+# okay decompiling C:\Users\hp\downloads\hidden_4.pyc
