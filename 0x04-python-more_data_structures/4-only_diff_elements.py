da4d8143dd00af818733a3b0c3d2d72fd19313f3
elisheba-beep <ekunife35@gmail.com>
2022-07-06 19:11:36 +0100

    only different

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    """print different"""
     diff1 = [i for i in set_1 if i not in set_2]
     diff2 = [i for i in set_2 if not i in set_1]
     diff = diff1 + diff2
