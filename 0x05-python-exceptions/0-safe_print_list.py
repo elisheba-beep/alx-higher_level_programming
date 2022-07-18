df8e8a9f651343aa93bfe506021d16ad23a8ac41
elisheba-beep <ekunife35@gmail.com>
2022-07-18 12:37:32 +0100

    index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-''''prints items in a list oon one line
-and returns the number of items printed'''
 def safe_print_list(my_list=[], x=0):
     number = 0
     for item in my_list[:x]:
