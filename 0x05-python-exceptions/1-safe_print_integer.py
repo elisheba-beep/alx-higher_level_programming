93b1cef8464b3c204dbbe21133e6348ab6568800
elisheba-beep <ekunife35@gmail.com>
2022-07-18 12:27:25 +0100

    true or false

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def safe_print_integer(value):
-        try:
-            print('{:d}'.format(value))
-            return True
-        except:
-            return False
+def safe_print_integer(value):
+    try:
+        print('{:d}'.format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
