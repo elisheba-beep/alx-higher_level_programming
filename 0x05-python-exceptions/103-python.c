80a5605aec2aa5ba36d36c6b9b8efba62870aa47
elisheba-beep <ekunife35@gmail.com>
2022-07-18 13:58:12 +0100

    python with c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#include <Python.h>
+
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
 /**
  * print_python_list - prints a python list
