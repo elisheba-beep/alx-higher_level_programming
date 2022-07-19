31c084295b185daef78fe3e40624e8784bfab31e
elisheba-beep <ekunife35@gmail.com>
2022-07-19 13:16:51 +0100

    a square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a square class"""
+
 class Square:
-    """"python3 -c 'print(__import__("my_module").MyClass.__doc__)
-    Represents a square created in the square class"""
+    """"Represents a square"""
     pass
