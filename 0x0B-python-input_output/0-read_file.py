f546d827a1b1a786930c227ee9ae4c03b32365d5
elisheba-beep <ekunife35@gmail.com>
2022-08-02 08:41:57 +0100

    reads a file to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#0-read_file.py
+"""defines a function
+"""
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout:
+
+    Args:
+        filename (str, optional): The path to the file needed to be opened.
+        Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
+        print(read_file)
