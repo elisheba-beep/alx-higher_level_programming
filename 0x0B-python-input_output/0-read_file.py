60f7b1582148fb74bea2a26e6a3a28608cfba124
elisheba-beep <ekunife35@gmail.com>
2022-08-02 12:14:39 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename=""):
         Defaults to "".
     """
     with open(filename, encoding="utf-8") as f:
-        read_file = f.read()
-        print(read_file)
+        print(f.read(), end="")
