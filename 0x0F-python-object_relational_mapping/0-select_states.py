bb825f9f5a3c32642ad27662b6822f4cfc79ae96
elisheba-beep <ekunife35@gmail.com>
2022-10-10 07:27:12 +0100

    documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+""" 
+Lists all states from the database hbtn_0e_0_usa.
+ Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name>
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """the main function to list the states in the database
-    Args(none)
-    Returns: none
-    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
